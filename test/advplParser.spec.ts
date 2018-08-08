0949a7d18f3b70abaf0d43bac856e6c731de1b1a
Daniel Mendes de Melo Sousa <daniel.mendes@sp01.local>
2018-08-08 12:04:24 -0300

    Criação de novos testes para funções estáticas, main e de usuário.

--- test/advplParser.spec.ts
+++ test/advplParser.spec.ts
@@ -28,6 +28,45 @@ describe('Function with parameters', () => {
   });
 });
 
+describe('Main function with parameters', () => {
+  it('should return 0 erros', () => {
+    const lexingResult = lexAdvpl("main function testing(param01,param02)\nreturn nil");
+    expect(lexingResult.errors).to.be.empty;
+    let tokenVocabulary = gettokenVocabulary();
+    let tokens = lexingResult.tokens
+    expect(tokenMatcher(tokens[0], tokenVocabulary["MainToken"])).to.be.true;
+    expect(tokenMatcher(tokens[1], tokenVocabulary["FunctionToken"])).to.be.true;
+    expect(tokenMatcher(tokens[2], tokenVocabulary["Identifier"])).to.be.true;
+
+  });
+});
+
+describe('User function with parameters', () => {
+  it('should return 0 erros', () => {
+    const lexingResult = lexAdvpl("user function testing(param01,param02)\nreturn nil");
+    expect(lexingResult.errors).to.be.empty;
+    let tokenVocabulary = gettokenVocabulary();
+    let tokens = lexingResult.tokens
+    expect(tokenMatcher(tokens[0], tokenVocabulary["UserToken"])).to.be.true;
+    expect(tokenMatcher(tokens[1], tokenVocabulary["FunctionToken"])).to.be.true;
+    expect(tokenMatcher(tokens[2], tokenVocabulary["Identifier"])).to.be.true;
+
+  });
+});
+
+describe('Static function with parameters', () => {
+  it('should return 0 erros', () => {
+    const lexingResult = lexAdvpl("static function testing(param01,param02)\nreturn nil");
+    expect(lexingResult.errors).to.be.empty;
+    let tokenVocabulary = gettokenVocabulary();
+    let tokens = lexingResult.tokens
+    expect(tokenMatcher(tokens[0], tokenVocabulary["StaticToken"])).to.be.true;
+    expect(tokenMatcher(tokens[1], tokenVocabulary["FunctionToken"])).to.be.true;
+    expect(tokenMatcher(tokens[2], tokenVocabulary["Identifier"])).to.be.true;
+
+  });
+});
+
 describe('Start function name with number', () => {
   it('should return 1 erros', () => {
     const lexingResult = lexAdvpl("function 666func()\nreturn");
